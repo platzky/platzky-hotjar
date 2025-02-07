6f5b2a42ed7f6e323d39ec30d34154d86c6985e3
Dawid-Sroka <94313700+Dawid-Sroka@users.noreply.github.com>
2025-02-07 18:05:39 +0100

    Cleanup for PR

--- tests/test_hotjar.py
+++ tests/test_hotjar.py
@@ -3,8 +3,6 @@ from typing import Any, Dict
 
 from platzky.platzky import create_app_from_config, Config
 
-from bs4 import BeautifulSoup, Tag
-
 def test_plugin_loader():
 
     secret_id_for_testing = "super_secret_id"
@@ -32,12 +30,7 @@ def test_plugin_loader():
     config_with_plugin = Config.model_validate(data_with_plugin)
     app_with_plugin = create_app_from_config(config_with_plugin)
     response = app_with_plugin.test_client().get("/")
-    # soup = BeautifulSoup(response.data, "html.parser")
-    # print(soup.prettify())
     decoded_response = response.data.decode()
     assert hotjar_function in decoded_response and secret_id_for_testing in decoded_response
-    # if hotjar_function in decoded_response and secret_id_for_testing in decoded_response:
-    #     print("\nsuper_secret_id\n")
-    #     print("super_secret_id")
     assert response.status_code == 404
 
