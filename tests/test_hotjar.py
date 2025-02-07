6c1ae03cbbe706e1bba08af914e71ddffdf56a58
Dawid-Sroka <94313700+Dawid-Sroka@users.noreply.github.com>
2025-02-07 18:00:12 +0100

    Work in progress with prints

--- tests/test_hotjar.py
+++ tests/test_hotjar.py
@@ -6,6 +6,9 @@ from platzky.platzky import create_app_from_config, Config
 from bs4 import BeautifulSoup, Tag
 
 def test_plugin_loader():
+
+    secret_id_for_testing = "super_secret_id"
+
     data_with_plugin: Dict[str, Any] = {
         "APP_NAME": "testingApp",
         "SECRET_KEY": "secret",
@@ -18,22 +21,23 @@ def test_plugin_loader():
                 "site_content": {"pages": []},
                 "plugins": [
                     {"name": "hotjar", "config": {"/page/test": "/page/test2",
-                                                  "ID": "super_secret_id"}}
+                                                  "ID": secret_id_for_testing}}
                 ],
             },
         },
     }
+
+    hotjar_function ="(function(h,o,t,j,a,r){"
+
     config_with_plugin = Config.model_validate(data_with_plugin)
     app_with_plugin = create_app_from_config(config_with_plugin)
     response = app_with_plugin.test_client().get("/")
-    soup = BeautifulSoup(response.data, "html.parser")
-    print(soup.prettify())
-    # assert soup.title is not None
-    # assert soup.title.string == "testing App Name"
-    # print(response.data.decode())
-    if "super_secret_id" in response.data.decode():
-        print("\nsuper_secret_id\n")
-        print("super_secret_id")
+    # soup = BeautifulSoup(response.data, "html.parser")
+    # print(soup.prettify())
+    decoded_response = response.data.decode()
+    assert hotjar_function in decoded_response and secret_id_for_testing in decoded_response
+    # if hotjar_function in decoded_response and secret_id_for_testing in decoded_response:
+    #     print("\nsuper_secret_id\n")
+    #     print("super_secret_id")
     assert response.status_code == 404
-    # assert response.location == "/page/test2"
 
