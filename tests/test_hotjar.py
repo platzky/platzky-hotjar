98b43cbd51ba2a1b0e4afdf28aec58b56a0d0a3f
Dawid-Sroka <94313700+Dawid-Sroka@users.noreply.github.com>
2025-02-07 17:53:15 +0100

    Work in progress as I left it

--- tests/test_hotjar.py
+++ tests/test_hotjar.py
@@ -3,6 +3,7 @@ from typing import Any, Dict
 
 from platzky.platzky import create_app_from_config, Config
 
+from bs4 import BeautifulSoup, Tag
 
 def test_plugin_loader():
     data_with_plugin: Dict[str, Any] = {
@@ -16,13 +17,23 @@ def test_plugin_loader():
             "DATA": {
                 "site_content": {"pages": []},
                 "plugins": [
-                    {"name": "hotjar", "config": {"/page/test": "/page/test2"}}
+                    {"name": "hotjar", "config": {"/page/test": "/page/test2",
+                                                  "ID": "super_secret_id"}}
                 ],
             },
         },
     }
     config_with_plugin = Config.model_validate(data_with_plugin)
     app_with_plugin = create_app_from_config(config_with_plugin)
-    response = app_with_plugin.test_client().get("/page/test", follow_redirects=False)
-    assert response.status_code == 301
-    assert response.location == "/page/test2"
+    response = app_with_plugin.test_client().get("/")
+    soup = BeautifulSoup(response.data, "html.parser")
+    print(soup.prettify())
+    # assert soup.title is not None
+    # assert soup.title.string == "testing App Name"
+    # print(response.data.decode())
+    if "super_secret_id" in response.data.decode():
+        print("\nsuper_secret_id\n")
+        print("super_secret_id")
+    assert response.status_code == 404
+    # assert response.location == "/page/test2"
+
