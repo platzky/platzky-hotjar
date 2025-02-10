db77708a64fb098388ce2e7fa7577c688b457fb4
Dawid Sroka <94313700+Dawid-Sroka@users.noreply.github.com>
2025-02-10 22:05:10 +0100

    Fix hotjar test (#2)

--- tests/test_hotjar.py
+++ tests/test_hotjar.py
@@ -5,6 +5,9 @@ from platzky.platzky import create_app_from_config, Config
 
 
 def test_plugin_loader():
+
+    secret_id_for_testing = "super_secret_id"
+
     data_with_plugin: Dict[str, Any] = {
         "APP_NAME": "testingApp",
         "SECRET_KEY": "secret",
@@ -16,13 +19,26 @@ def test_plugin_loader():
             "DATA": {
                 "site_content": {"pages": []},
                 "plugins": [
-                    {"name": "hotjar", "config": {"/page/test": "/page/test2"}}
+                    {
+                        "name": "hotjar",
+                        "config": {
+                            "/page/test": "/page/test2",
+                            "ID": secret_id_for_testing,
+                        },
+                    }
                 ],
             },
         },
     }
+
+    hotjar_function = "(function(h,o,t,j,a,r){"
+
     config_with_plugin = Config.model_validate(data_with_plugin)
     app_with_plugin = create_app_from_config(config_with_plugin)
-    response = app_with_plugin.test_client().get("/page/test", follow_redirects=False)
-    assert response.status_code == 301
-    assert response.location == "/page/test2"
+    response = app_with_plugin.test_client().get("/")
+    assert response.status_code == 404
+    decoded_response = response.data.decode()
+    assert (
+        hotjar_function in decoded_response
+        and secret_id_for_testing in decoded_response
+    )
